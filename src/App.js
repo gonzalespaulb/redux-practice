4a8a4d9d8ba4b22159ee22fdcd24708ce235a737
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2021-09-15 15:30:24 -0700

    Practice API call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import Post from './components/Post';
+import react, {useEffect, useState} from 'react';
 
 function App() {
+
+  //.............................................................................................. How to fetch API 
+  const [post, setPost] = useState([]);
+
+  const fetchAPI = async () => {
+    const fetchedAPI = await fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(json => json)
+
+    return fetchedAPI;
+  }
+
+  const getFetchedData = async () => {
+    const fetchedData = await fetchAPI();
+    setPost(fetchedData);
+  }
+
+  useEffect(() => {
+    getFetchedData();
+  })
+
+//.............................................................................................. How to fetch API 
+
+  const mappedData = () => {
+    const test = post.map((data, index) => {
+      return <h1 key={index}>{data.title}</h1>
+    })
+
+    return test;
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Post></Post>
+      {mappedData()}
     </div>
   );
 }
---
+++ src/components/Post.js
@@ -0,0 +1,9 @@
+const Post = () => {
+    return (
+        <div>
+            <h1>Post</h1>
+        </div>
+    )
+}
+
+export default Post;
